95573a5bfe8611b1a2b977c4565d3d56d936afe8
Sushil Pokhrel <sushil.pokhrel@deerwalk.edu.np>
2017-05-21 14:49:16 +0545

    Master pafe created

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Swotah Travel | @yield('title')</title>
+    {{--Material Design CSS--}}
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{--Material Icons--}}
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material-grid.css">
+    @yield('links')
+    @yield('metatags')
+</head>
+<body>
+@yield('content')
+{{--Scripts--}}
+<script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+@yield('scripts')
+</body>
+
+</html>
+
+
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,35 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{--Extending the master page--}}
+@extends('layouts.master')
+{{--Section for individual title for a page--}}
+@section('title')Index Page @endsection
+{{--Title end--}}
+{{--Place for css links--}}
+    {{--put links here--}}
+{{--End css--}}
 
-        <title>Laravel</title>
+{{--Section for Meta Tags SEO Purposes yield('metatags')--}}
+    {{--Put tags here--}}
+{{--End section for meta tags--}}
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+{{--Section start for content--}}
+@section('content')
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer">
+        <div class="mdl-layout__drawer">
+            <span class="mdl-layout-title">HTML5 Tutorial</span>
+            <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Home</a>
+                <a class="mdl-navigation__link" href="">About</a>
+            </nav>
         </div>
-    </body>
-</html>
+        <main class="mdl-layout__content">
+            <div class="page-content" style="padding-left:100px;">Hello World!</div>
+        </main>
+    </div>
+
+    {{--place for scripts--}}
+
+    {{--put scripts here--}}
+
+    {{--end scripts--}}
+@endsection
