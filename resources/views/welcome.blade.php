6c6cad4e6eb586e9358eeefd439c6d390eec1f96
Abhushan Gautam <carefreeav09@gmail.com>
2017-05-22 15:09:59 +0545

    nav layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,33 +14,8 @@
 {{--Section start for content--}}
 @section('content')
     {{--Outer Layout --}}
-        <div class="mdl-layout mdl-layout__content">
-            <div class = "mdl-grid heading-panel">
-                <div class = "mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--hide-phone logo" >
-                    <img src="{{url('public_images/logo_swotah.png')}}">
-                </div>
-                <div class = "mdl-cell mdl-cell--hide-desktop mdl-cell--hide-tablet mdl-cell--4-col-phone logo" style = "text-align:center;" >
-                    <img src="{{url('public_images/logo_swotah.png')}}">
-                </div>
 
-                    <div class = "mdl-cell mdl-cell--6-col mdl-cell--2-col-tablet mdl-cell--hide-phone"></div>
-                    <a href="" class =  "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">account_circle</i>
-                    </a>
-                    <a href="" class =  "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">assignment</i>
-                    </a>
-                    <a href="" class = "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">contact_phone</i>
-                    </a>
-                    <a href="" class = "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">shopping_basket</i>
-                    </a>
-
-
-
-            </div>
-        </div>
+    @include('layouts.nav');
 
 
 
