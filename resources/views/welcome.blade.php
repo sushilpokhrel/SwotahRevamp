ed46973f3e532f842ab229767b46ea3a2d9131fe
Sushil Pokhrel <sushil.pokhrel@deerwalk.edu.np>
2017-05-22 16:19:23 +0545

    My changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,31 +14,33 @@
 {{--Section start for content--}}
 @section('content')
     {{--Outer Layout --}}
-        <div class="mdl-layout mdl-layout__content">
+        <div class="mdl-layout mdl-layout__content" >
+            <div class="index-image">
+                <img src="{{url('public_images/2.jpg')}}" alt="">
+            </div>
             <div class = "mdl-grid heading-panel">
                 <div class = "mdl-cell mdl-cell--2-col mdl-cell--2-col-tablet mdl-cell--hide-phone logo" >
                     <img src="{{url('public_images/logo_swotah.png')}}">
                 </div>
-                <div class = "mdl-cell mdl-cell--hide-desktop mdl-cell--hide-tablet mdl-cell--4-col-phone logo" style = "text-align:center;" >
-                    <img src="{{url('public_images/logo_swotah.png')}}">
-                </div>
-
-                    <div class = "mdl-cell mdl-cell--6-col mdl-cell--2-col-tablet mdl-cell--hide-phone"></div>
-                    <a href="" class =  "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">account_circle</i>
-                    </a>
-                    <a href="" class =  "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">assignment</i>
-                    </a>
-                    <a href="" class = "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">contact_phone</i>
-                    </a>
-                    <a href="" class = "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
-                        <i class="material-icons">shopping_basket</i>
-                    </a>
 
 
-
+                <div class = "mdl-cell mdl-cell--6-col mdl-cell--2-col-tablet mdl-cell--hide-phone"></div>
+                <a  id="account" href="" class =  "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
+                    <div class="mdl-tooltip mdl-tooltip--large" for="account">My Account</div>
+                    <i class="material-icons">account_circle</i>
+                </a>
+                <a href="" id = "brochure" class =  "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
+                    <i class="material-icons">assignment</i>
+                    <div class="mdl-tooltip mdl-tooltip--large" for="brochure">Brochure</div>
+                </a>
+                <a href="" id = "contact" class = "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
+                    <i class="material-icons">contact_phone</i>
+                    <div class="mdl-tooltip mdl-tooltip--large" for="contact">(+977) 01-4404750</div>
+                </a>
+                <a href="" id = "bucketlist" class = "mdl-cell mdl-cell--1-col mdl-cell--1-col-tablet mdl-cell--1-col-phone icon">
+                    <i class="material-icons">favorite_border</i>
+                    <div class="mdl-tooltip mdl-tooltip--large" for="bucketlist">Bucket List</div>
+                </a>
             </div>
         </div>
 
@@ -46,7 +48,6 @@
 
     {{--place for scripts--}}
 
-    {{--put scripts here--}}
 
     {{--end scripts--}}
 @endsection
